695fbe63f635a571a6421747630ecd065f997a87
Binod Rawat <140739349+BinodRawat1@users.noreply.github.com>
2025-02-22 15:50:26 +0545

    Add files via upload

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Polygon
+{
+    // method to render a shape
+    public virtual void render()
+    {
+        Console.WriteLine("Displaying Polygon...");
+    }
+}
+
+class Square : Polygon
+{
+    // overriding render() method 
+    public override void render()
+    {
+        Console.WriteLine("Displaying Square...");
+    }
+}
+class myProgram
+{
+    public static void Main()
+    {
+        // obj1 is the object of Polygon class
+        Polygon obj1 = new Polygon();
+
+        // calls render() method of Polygon Superclass 
+        obj1.render();
+
+        // here, obj1 is the object of derived class Square 
+        obj1 = new Square();
+
+        // calls render() method of derived class Square
+        obj1.render();
+    }
+}
