69de317ffe5172e8a3940ffddc7d269b959d73ef
Binod Rawat <140739349+BinodRawat1@users.noreply.github.com>
2025-02-22 16:17:45 +0545

    Add files via upload

---
+++ jsonlab11/jsonlab11/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿// Controllers/ProductsController.cs
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ProductAPI.Models;
+
+namespace ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // Sample list of products
+        private List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Laptop", Price = 1200.00m },
+            new Product { Id = 2, Name = "Smartphone", Price = 800.00m },
+            new Product { Id = 3, Name = "Tablet", Price = 600.00m }
+        };
+
+        // GET: api/products
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(products); // Returns the list of products in JSON format
+        }
+    }
+}
